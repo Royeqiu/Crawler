e9b9be28f58303437c4a5ffb716418b8a901c9f8
roye <roye1202@hotmail.com.tw>
2017-03-28 22:20:31 +0800

    update the HttpLTS and searchEngineResultDomParser

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 package main;
 
+import DOMTempateParser.BasicSearcgEngineResultUrlFetcher;
+import DOMTempateParser.YahooAnswerResultWebUrlFetcher;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import websiteProcess.urlTable;
+
+import java.io.IOException;
+
+
 public class Main {
-
+    static urlTable urlTable;
     public static void main(String[] args) {
-
-
+        BasicSearcgEngineResultUrlFetcher urlFetcher= new YahooAnswerResultWebUrlFetcher();
+        urlFetcher.getSearchEngineResult("https://answers.search.yahoo.com/search?b=11&p=wine");
+        urlFetcher.parseSearchEngineResult();
     }
 }
